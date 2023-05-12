fe21783c3d7ebc20a4d81ec73a0f5296e2a69654
Daniel Carvalho <dl.carvalho@ua.pt>
2023-05-12 20:51:19 +0100

    DM-37: Setting up the SpringBoot project and docker compose

---
+++ DropMate_Backend/src/test/java/tqs/dropmate/dropmate_backend/DropMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.dropmate.dropmate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropMateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
